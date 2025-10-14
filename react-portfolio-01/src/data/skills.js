1fa17c6864f1a37b2bf76f84190e77aaf11854b8
GeoffreyMwashumbe254 <geoffreymwashumbe@gmail.com>
2025-10-14 09:40:17 +0300

    Added My Skills

---
+++ react-portfolio-01/src/data/skills.js
@@ -0,0 +1,6 @@
+export const skills = [
+    {category: "Programming Languages", items: ["JavaScript", "Python", "Solidity"]},
+    {category: "Blockchain Technology", items: ["Tokenomics", "Consultancy", "Smart Contracts", "DApps", "DeFi", "NFTs"]},
+    {category: "Web Development", items: ["React", "Node.js", "Express", "NextJS", "HTML5", "CSS3", "Tailwind CSS", "WordPress"]},
+    {category: "Databases", items: ["MongoDB", "MySQL", "Supabase", "PostgreSQL"]},
+]
