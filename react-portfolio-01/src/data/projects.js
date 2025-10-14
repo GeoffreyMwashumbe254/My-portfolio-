2b4445c1ce832beb00fd78dc90e00b1cbba535e1
GeoffreyMwashumbe254 <geoffreymwashumbe@gmail.com>
2025-10-14 09:33:08 +0300

    Added Projects section

---
+++ react-portfolio-01/src/data/projects.js
@@ -0,0 +1,22 @@
+export const projects = [
+    {
+        title: "Asal MicroFincance",
+        description: "A microfinance platform to support small businesses.",
+        tech: ["Wordpress", "Custom Plugins", "Elementor", "Customized Theme"],
+        link: "https://asalmicrofinance.com/"
+    },
+
+    {
+        title: "African Academy of Engineering",
+        description: "An educational platform for engineering professionals in Africa.",
+        tech: ["Wordpress", "Custom Plugins", "Elementor", "Customized Theme"],
+        link: "https://aae-africa.org/"
+    },
+
+    {
+        title: "Techzilla Limited",
+        description: "Technology solutions provider's corporate website.",
+        tech: ["Wordpress", "Custom Plugins", "Elementor", "Customized Theme"],
+        link: "https://techzilla.co.ke/"
+    },
+];
